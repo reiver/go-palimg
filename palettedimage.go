af132c2b181cc76b0c3eab46b63cfd546b1fbd77
Charles Iliya Krempeaux <iamreiver@gmail.com>
2024-04-11 23:00:50 -0700

    palimg.internalPalettedImage

---
+++ palettedimage.go
@@ -0,0 +1,53 @@
+package palimg
+
+import (
+	"image"
+	"image/color"
+)
+
+type internalPalettedImage struct {
+	palimg PalImg
+	rectangle image.Rectangle
+	palette []color.Color
+}
+
+var _ image.PalettedImage = internalPalettedImage{}
+
+func (receiver internalPalettedImage) At(x, y int) color.Color {
+	if len(receiver.palette) <= 0 {
+		return nil
+	}
+
+	var colorindex uint8 = receiver.ColorIndexAt(x, y)
+	var ci int = int(colorindex)
+
+	if ci < 0 || len(receiver.palette) <= ci {
+		return nil
+	}
+
+	return receiver.palette[colorindex]
+}
+
+
+func (receiver internalPalettedImage) Bounds() image.Rectangle {
+	return receiver.rectangle
+}
+
+func (receiver internalPalettedImage) ColorIndexAt(x, y int) uint8 {
+	var point image.Point = image.Pt(x,y)
+
+	if !point.In(receiver.rectangle) {
+		return 0
+	}
+
+	var palimg PalImg = receiver.palimg
+	if nil == palimg {
+		return 0
+	}
+
+	return palimg.ColorIndexAt(x,y)
+}
+
+func (receiver internalPalettedImage) ColorModel() color.Model {
+	return color.Palette(receiver.palette)
+}
